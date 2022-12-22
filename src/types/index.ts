3198769634f8275b5755a51a88965686f31a6aa8
Dmitriy <shkredovdmitriy@gmail.com>
2022-12-22 22:55:32 +0300

    feat: add types, paths

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export type TProductItem = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+};
+
+export type TFilter = {
+  brands: string[];
+  categories: string[];
+  priceMin: number;
+  priceMax: number;
+};
