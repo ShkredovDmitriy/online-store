010e812233d7adc9169bf7ea04d54f2371c9f7b1
Dmitriy <shkredovdmitriy@gmail.com>
2022-12-23 22:19:06 +0300

    feat: add filter to catalog

--- src/pages/PageCatalog/PageCatalog.tsx
+++ src/pages/PageCatalog/PageCatalog.tsx
@@ -1,17 +1,43 @@
 import React from "react";
+import "./pageCatalog.scss";
 import { TProductItem } from "types";
 import { productsList } from "data";
 import { ProductFilter, ProductCard } from "components";
-import "./pageCatalog.scss";
+import { useSelector, RootState } from "store";
 
 export const PageCatalog = () => {
+  const filterCategory = useSelector(
+    (state: RootState) => state.filterCategory
+  ) as string[];
+
+  const filterBrand = useSelector(
+    (state: RootState) => state.filterBrand
+  ) as string[];
+
+  console.log(filterCategory);
+  console.log(filterBrand);
+
+  let filteredList: TProductItem[] = [...productsList];
+
+  if (filterCategory.length > 0)
+    filteredList = filteredList.filter((productItem: TProductItem) =>
+      filterCategory.includes(productItem.category)
+    );
+
+  if (filterBrand.length > 0)
+    filteredList = filteredList.filter((productItem: TProductItem) =>
+      filterBrand.includes(productItem.brand)
+    );
+
   return (
     <main className="page-main">
       <ProductFilter />
       <section className="catalog">
-        {productsList.map((productItem: TProductItem) => (
-          <ProductCard product={productItem} key={productItem.id} />
-        ))}
+        {filteredList.length > 0 &&
+          filteredList.map((productItem: TProductItem) => (
+            <ProductCard product={productItem} key={productItem.id} />
+          ))}
+        {filteredList.length === 0 && <>No product found</>}
       </section>
     </main>
   );
