284231af9455f2e73ac7c39083169f4205a9c62a
Dmitriy <shkredovdmitriy@gmail.com>
2022-12-24 16:26:47 +0300

    feat: add price

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -4,13 +4,14 @@ import { Link } from "react-router-dom";
 import "./productCard.scss";
 
 export const ProductCard = ({ product }: { product: TProductItem }) => {
-  const { id, category, title, brand } = product;
+  const { id, category, title, brand, price } = product;
 
   return (
     <div className="product-card">
       <div className="product-card__category">{category}</div>
       <div className="product-card__title">{title}</div>
       <div className="product-card__brand">{brand}</div>
+      <div className="product-card__price">{price}</div>
       <Link className="product-card__button-info" to={`/product/${id}`}>
         Details
       </Link>
