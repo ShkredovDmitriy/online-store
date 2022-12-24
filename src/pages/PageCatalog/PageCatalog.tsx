65fe1cb632602b45afa2fafc4e56cd39f2207ba8
Dmitriy <shkredovdmitriy@gmail.com>
2022-12-24 14:06:08 +0300

    feat: update catalog

--- src/pages/PageCatalog/PageCatalog.tsx
+++ src/pages/PageCatalog/PageCatalog.tsx
@@ -14,8 +14,8 @@ export const PageCatalog = () => {
     (state: RootState) => state.filterBrand
   ) as string[];
 
-  console.log(filterCategory);
-  console.log(filterBrand);
+  console.log("FILTER CATEGORY", filterCategory);
+  console.log("FILTER BRAND", filterBrand);
 
   let filteredList: TProductItem[] = [...productsList];
 
@@ -33,11 +33,33 @@ export const PageCatalog = () => {
     <main className="page-main">
       <ProductFilter />
       <section className="catalog">
-        {filteredList.length > 0 &&
-          filteredList.map((productItem: TProductItem) => (
-            <ProductCard product={productItem} key={productItem.id} />
-          ))}
-        {filteredList.length === 0 && <>No product found</>}
+        <div className="catalog__panel">
+          <span>
+            <select name="sort">
+              <option value="sort-title" disabled>
+                Sort options:
+              </option>
+              <option value="price-ASC">Sort by price ASC</option>
+              <option value="price-DESC">Sort by price DESC</option>
+              <option value="rating-ASC">Sort by rating ASC</option>
+              <option value="rating-DESC">Sort by rating DESC</option>
+              <option value="discount-ASC">Sort by discount ASC</option>
+              <option value="discount-DESC">Sort by discount DESC</option>
+            </select>
+          </span>
+          <span>Found: {filteredList.length}</span>
+          <span>
+            <input type="radio" name="view" defaultChecked />
+            <input type="radio" name="view" />
+          </span>
+        </div>
+        <div className="catalog__prod-list">
+          {filteredList.length > 0 &&
+            filteredList.map((productItem: TProductItem) => (
+              <ProductCard product={productItem} key={productItem.id} />
+            ))}
+          {filteredList.length === 0 && <>No products found</>}
+        </div>
       </section>
     </main>
   );
