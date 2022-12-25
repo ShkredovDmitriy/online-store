5e3d035cdfb6957225461e310f0d1db77517dfd4
Dmitriy <shkredovdmitriy@gmail.com>
2022-12-26 00:07:43 +0300

    feat: add preview image & Add to card button

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -4,16 +4,19 @@ import { Link } from "react-router-dom";
 import "./productCard.scss";
 
 export const ProductCard = ({ product }: { product: TProductItem }) => {
-  const { id, category, title, brand, price, discountPercentage } = product;
+  const { id, category, title, brand, price, discountPercentage, thumbnail } =
+    product;
 
   return (
     <div className="product-card">
+      <img className="product-card__preview" src={thumbnail} alt="Preview" />
       <div className="product-card__category">{category}</div>
       <div className="product-card__title">{title}</div>
       <div className="product-card__brand">{brand}</div>
       <div className="product-card__price">
         {price} / -{discountPercentage}%
       </div>
+      <button className="product-card__button-add">Add to cart</button>
       <Link className="product-card__button-info" to={`/product/${id}`}>
         Details
       </Link>
