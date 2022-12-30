8210f066bf87e77edfcae27a0cff9446a463eee5
Полина Куксова <pakuksova@gmail.com>
2022-12-30 21:00:42 +0300

    refactor: change pass for logo-img in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../Style/main.scss";
-import rsLogo from "../Img/rs_school.svg"
-import gHLogo from "../Img/github-icon.svg"
+import rsLogo from "../../assets/Img/rs_school.svg"
+import gHLogo from "../../assets/Img/github-icon.svg"
 
 export const Footer = () => {
   return <footer className="footer">
