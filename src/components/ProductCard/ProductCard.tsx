8f6bbcaca3185f5e4be0598848c19bf82c948317
Полина Куксова <pakuksova@gmail.com>
2023-01-02 15:38:37 +0300

    fix: delete card-info because it is optional info

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -20,14 +20,14 @@ export const ProductCard = ({ product }: { product: TProductItem }) => {
     <div className="product-card">
       <img className="product-card__preview" src={thumbnail} alt="Preview" />
       <h2 className="product-card__title">{title}</h2>
-      <div className="product-card__info">
+      {/* <div className="product-card__info">
         <p>Category: {category}</p>
         <p>Brand: {brand}</p>
         <p>Rating: {rating}</p>
         <p>Stock: {stock}</p>
       </div>
       <div className="product-card__category">{category}</div>
-      <div className="product-card__brand">{brand}</div>
+      <div className="product-card__brand">{brand}</div> */}
       <div className="product-card__price">
         {price} / -{discountPercentage}%
       </div>
