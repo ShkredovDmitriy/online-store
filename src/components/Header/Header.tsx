559b5f6276d73b737b12d2dee1424b58ca785b54
Полина Куксова <pakuksova@gmail.com>
2023-01-04 22:44:35 +0300

    feat: add header style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,15 @@
 import React from "react";
-import "./header.scss";
+import "../Style/main.scss";
+import  store from "../../assets/Img/iconstore.png";
+import  bag from "../../assets/Img/bag.png";
 
 export const Header = () => {
-  return <header className="header"></header>;
+  return <header className="header">
+    <div className="header__logo">
+      <img src={store} alt="store" />
+      <div className="header__title">Online store</div>
+    </div>
+    <div className="header__total-price">Card total: </div>
+    <img src={bag} alt="bag" />
+  </header>;
 };
