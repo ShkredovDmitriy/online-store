430db933a8d6b6a961636be000d6ab2cbdfd538f
Полина Куксова <pakuksova@gmail.com>
2023-01-09 23:21:08 +0300

    fix: page in cart

--- src/pages/PageCart/PageCart.tsx
+++ src/pages/PageCart/PageCart.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import "../../components/Style/main.scss";
 import { TCartItem} from "types";
 import { Button } from '../../components/Button/button';
@@ -7,7 +7,6 @@ import { promoConsts, TPromoCode } from "../../components/Promo/promoConsts"
 import { ProductCardInCart } from '../../components/ProductCardInCart/ProductCardInCart';
 import { action, useSelector, RootState } from "store";
 import { useDispatch } from "react-redux";
-import NumericInput from 'react-numeric-input';
 
 
 export const PageCart = () => {
@@ -48,21 +47,65 @@ export const PageCart = () => {
 
     };
 
+    //пагинация
+
+    // задаем колличество карточек продукта на одной странице
+    const [pageSize, setPageSize] = useState<number>(3);
+    
+    // создаем матрицу , где число столбцов - страницы, 
+        //а число строк - число товаров на странице и pageSize, 
+        //введенное пользователем в NumericInput
+    const pageArr = (arr: TCartItem[], size: number) => 
+        Array.from({ length: Math.ceil(arr.length / size)}, (v, i) =>  
+        arr.slice(i * size, i * size + size));
+    const cartItemsPages = pageArr(cartItems, pageSize);
+    
+    
+    // задаем колличество страниц для span
+    const [pageNum, setPageNum] = useState<number>(1);
+    const addPage = () => {
+        if (pageNum < cartItemsPages.length) {
+            setPageNum(pageNum + 1)
+        }
+    };
+    const removePage = () => {
+        if (pageNum > 1) {
+            setPageNum(pageNum - 1)
+        }
+    };
+
+    const changePageSize = (newSize: number) => {
+        const n = Math.ceil(cartItems.length / newSize);
+        if (pageNum > n) {
+            setPageNum(n)
+        }
+        setPageSize(newSize);
+    }
+
+    //console.log(Math.floor(cartItems.length / pageSize));
     return (
       <main className="page-cart">
-        <div className="inner">
+        {!cartItems.length 
+                ? (<p> Cart is Empty </p>) 
+                : (<div className="inner">
         <div className="cart">
             <div className="cart__header">
                 <div className="cart__header-title">Products in cart</div>
                 <div className="cart__header-control">
                     Limit
-                    <NumericInput className="cart__header-control-input" min={1} max={cartItems.length} value={3}/>
+                    <input type="number" className="cart__header-control-input" min={1} max={cartItems.length} value={pageSize} onChange={event => changePageSize(Number(event.target.value))}     />
+                </div>
+                <div className="cart__header-page">
+                    <span>Page:</span>
+                    <Button type="switch-btn" backColor="#B4E907" onClick={() => removePage()}>{'<'}</Button>
+                    <span>{pageNum} / {cartItemsPages.length}</span>
+                    <Button type="switch-btn" backColor="#B4E907" onClick={() => addPage()}>{'>'}</Button>
                 </div>
             </div>
             <div className="cart__product">
-              {filteredList.map((cartItem: TCartItem, i) => (
-                <ProductCardInCart addItem={addItem} removeItem={removeItem} item={cartItem} order={i + 1} key={cartItem.id}  />
-              ))}
+                {cartItemsPages[pageNum - 1]?.map((cartItem: TCartItem, i) => (
+                    <ProductCardInCart addItem={addItem} removeItem={removeItem} item={cartItem} order={(i + 1) + (pageNum - 1) * pageSize} key={cartItem.id}  />
+                 ))}
             </div>
         </div>
         <div className="total">
@@ -79,9 +122,9 @@ export const PageCart = () => {
             <div className="total__promo">
                 <Promo />
             </div>
-            <Button type="primary-btn" backColor="#ffffff" onClick={() => console.log('1')} >Buy now</Button>
+            <Button type="primary-btn" backColor="#ffffff" onClick={() => console.log('1')}>Buy now</Button>
         </div>
-        </div>
+                 </div>)}
       </main>
     );
-  };
+};
